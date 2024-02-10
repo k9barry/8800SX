f02550ff68de1f91fde9e3519f23f634b2df1048
K9 Barry <k9barry@gmail.com>
2024-02-10 10:32:59 -0500

    WIP

--- data/web/index.php
+++ data/web/index.php
@@ -12,30 +12,48 @@
         $(document).ready(function() {
             $("#fileuploader").uploadFile({
                 url: "./jquery-upload-file/php/upload.php",
-                multiple: "true",
+                autoSubmit: false,
+                multiple: true,
                 returnType: "json",
                 fileName: "myfile",
                 upload_path: '../../uploads',
                 allowedTypes: "txt",
-                multiple:true,
+                multiple: true,
                 onLoad: function(obj) {
-                    $("#eventsmessage").html($("#eventsmessage").html() + "<br/>Widget Loaded:");
+                    $.ajax({
+                        cache: false,
+                        url: "./jquery-upload-file/php/load.php",
+                        dataType: "json",
+                        success: function(data) {
+                            for (var i = 0; i < data.length; i++) {
+                                obj.createProgress(data[i]);
+                            }
+                        }
+                    });
                 },
-                onSubmit: function(files) {
-                    $("#eventsmessage").html($("#eventsmessage").html() + "<br/>Submitting:" + JSON.stringify(files));
-                    //return false;
+                onSelect: function(files) {
+                    uploadObj.startUpload();
+                    return true; //to allow file submission.
                 },
+                /*onSubmit: function(files) {
+                    //files : List of files to be uploaded
+                    //return false;   to stop upload
+                    var_dump(files);
+                    uploadObj.startUpload();
+                    return true;
+                },*/
                 onSuccess: function(files, data, xhr, pd) {
-                    $("#eventsmessage").html($("#eventsmessage").html() + "<br/>Success for: " + JSON.stringify(data));
+                    $("#fileuploader").html($("#fileuploader").html() + "<br/>Success for: " + JSON.stringify(data));
                 },
                 afterUploadAll: function(obj) {
-                    $("#eventsmessage").html($("#eventsmessage").html() + "<br/>All files are uploaded");
+                    $("#fileuploader").html($("#fileuploader").html() + "<br/>All files are uploaded");
+                    //function to check of file exists in db filename and file into database.
                 },
                 onError: function(files, status, errMsg, pd) {
-                    $("#eventsmessage").html($("#eventsmessage").html() + "<br/>Error for: " + JSON.stringify(files));
+                    $("#fileuploader").html($("#fileuploader").html() + "<br/>Error for: " + JSON.stringify(files));
                 },
                 onCancel: function(files, pd) {
-                    $("#eventsmessage").html($("#eventsmessage").html() + "<br/>Canceled  files: " + JSON.stringify(files));
+                    $("#fileuploader").html($("#fileuploader").html() + "<br/>Canceled  files: " + JSON.stringify(files));
                 }
             });
         });
