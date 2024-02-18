a4c90014b06e32da821632aa6634daddb4994f00
K9 Barry <k9barry@gmail.com>
2024-02-18 16:38:31 -0500

    Fixes password secret in connection file - rotated secret

--- data/web/connection.php
+++ data/web/connection.php
@@ -3,7 +3,7 @@
 // Set mysql connect variables
 $host = "db";
 $username = "viavi";
-$password = "8800SX";
+$password = trim(file_get_contents(getenv("DB_PASSWORD_FILE")));
 $database = "viavi";
 
 $connection = new mysqli($host, $username, $password, $database);
