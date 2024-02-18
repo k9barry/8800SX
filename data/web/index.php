70fb20b66816a2a305ea0c8873dea51760cc96e7
K9 Barry <k9barry@gmail.com>
2024-02-18 11:41:12 -0500

    Working version of prepared statements

--- data/web/index.php
+++ data/web/index.php
@@ -22,13 +22,17 @@
 </style>
 
 <?php
-$count = 0;
+$count = 0;  //Get count of successfully uploaded records
 $msg = "";
 $dirname = "uploads";
 array_map('unlink', glob("$dirname/*")); // Remove all files from upload folder
 include('connection.php');
 
-// Get filenames from DB
+// Set INSERT statement and prepare
+$sql = "INSERT INTO alignments (datetime, model, serial, file, filename) VALUES (?, ?, ?, ?, ?)";
+$statement = $connection->prepare($sql);
+
+// Get all filenames from DB and place in array $db
 $sqlFind = 'SELECT `filename` FROM `alignments`';
 $result = mysqli_query($connection, $sqlFind);
 $db = []; // create empty array
@@ -37,6 +41,8 @@ while ($row = mysqli_fetch_row($result)) {
 }
 
 if (isset($_REQUEST['file-upload'])) {
+
+  // Loop thru upladed files and get info to put into DB
   for ($i = 0; $i < count($_FILES['multiple_files']['name']); $i++) {
     $filename[] = basename($_FILES['multiple_files']['name'][$i]);
 
@@ -45,50 +51,49 @@ if (isset($_REQUEST['file-upload'])) {
     $path_ext = $path_part['extension'];
     if ($path_ext <> "txt") {
       $msg .= "File " . $filename[$i] . " does not end in '.txt' unable to upload<br>";
-      continue;
-    }
-
-    // Save the remaining files to the upload folder
-    $uploadfile = $_FILES['multiple_files']['tmp_name'][$i];
-    $targetpath = $dirname . "/" . $filename[$i];
-    move_uploaded_file($uploadfile, $targetpath);
-
-    // Check if filename matches array of DB names
-    if (in_array($filename[$i], $db)) {
-      $msg .= "File " . $filename[$i] . " already exists in DB unable to upload<br>";
-      continue;
-    }
-
-    // Fix the datetime from the filename to insert into DB
-    $filevalue = explode('-', $filename[$i]);
-    $month = substr($filevalue[2], 0, 2);
-    $day = substr($filevalue[2], 2, 2);
-    $year = substr($filevalue[2], 4, 4);
-    $hour = substr($filevalue[3], 0, 2);
-    $minute = substr($filevalue[3], 2, 2);
-    $second = substr($filevalue[3], 4, 2);
-    $datetime = "" . $year . "-" . $month . "-" . $day . " " . $hour . ":" . $minute . ":" . $second . "";
-
-    // Set more variables to insert into DB
-    $model = $filevalue[0];
-    $serial = $filevalue[1];
-
-    // Read the file contents to variable once it been uploaded for insert into DB
-    $file_contents = file_get_contents($targetpath);
-
-    $query = "INSERT INTO alignments (datetime, model, serial, file, filename) VALUES ('$datetime', '$model', '$serial', '$file_contents', '$filename[$i]')";
-    $insert_query = mysqli_query($connection, $query);
-
-    if ($insert_query > 0) {
-      $count++;
-      $msg .= "File " . $filename[$i] . " uploaded successfuly<br>";
     } else {
-      $msg .= " File" . $filename[$i] . " Error!<br>";
+
+      // Save the remaining files to the upload folder
+      $tempname = $_FILES['multiple_files']['tmp_name'][$i];
+      $targetpath = $dirname . "/" . $filename[$i];
+      move_uploaded_file($tempname, $targetpath);
+
+      // Check if uploaded filename is not in array of DB names
+      if (!in_array($filename[$i], $db)) {
+
+        // Fix the datetime from the filename to insert into DB
+        $filevalue = explode('-', $filename[$i]);
+        $month = substr($filevalue[2], 0, 2);
+        $day = substr($filevalue[2], 2, 2);
+        $year = substr($filevalue[2], 4, 4);
+        $hour = substr($filevalue[3], 0, 2);
+        $minute = substr($filevalue[3], 2, 2);
+        $second = substr($filevalue[3], 4, 2);
+
+        // Set variables to insert into DB
+        $datetime = "" . $year . "-" . $month . "-" . $day . " " . $hour . ":" . $minute . ":" . $second . "";
+        $model = $filevalue[0];
+        $serial = $filevalue[1];
+        $file_contents = file_get_contents($targetpath); // Read the file contents to string variable once it been uploaded for insert into DB
+
+        // Bind the statement and execute
+        $statement->bind_param("sssss", $datetime, $model, $serial, $file_contents, $filename[$i]);
+        $statement->execute();  // Execute the mysql statement
+
+        if ($statement) {
+          $count++;
+          $msg .= "<font color='green'>File " . $filename[$i] . " uploaded successfuly</font><br>";
+        } else {
+          $msg .= " File" . $filename[$i] . " Error!<br>";
+        }
+      } else {
+        $msg .= "File " . $filename[$i] . " already exists in DB unable to upload<br>";
+      }
     }
   }
 }
 if ($count > 0) {
-  $msg .= "<br><b>!!!!! " . $count . " files uploaded successfuly to the DB !!!!!</b><br>";
+  $msg .= "<br><font color='green'>!!!!! " . $count . " files uploaded successfuly to the DB !!!!!</font><br>";
 }
 ?>
 
