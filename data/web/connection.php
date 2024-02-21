d3f287fcf6fad8b3107d016e60c49503cd0c85de
K9 Barry <k9barry@gmail.com>
2024-02-20 21:46:06 -0500

    added phpdoc documentation

--- data/web/connection.php
+++ data/web/connection.php
@@ -1,6 +1,16 @@
 <?php
+/**
+ * Connection file connection.php
+ * 
+ * Set mysql connection variables to connect to mySql DB
+ * 
+ * 1. String $host Host container name typically "db"
+ * 2.String $username Username set when mySql DB was created
+ * 3. String $password Password set in secrets/db_password.txt file
+ * 4. String $database Database name set when mySql DB was created
+ * 
+ */
 
-// Set mysql connect variables
 $host = "db";
 $username = "viavi";
 $password = trim(file_get_contents(getenv("DB_PASSWORD_FILE")));
